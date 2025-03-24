3ad42dbe24b8afa13c6a40cd3ed588ab1dba8af2
Aqsa-12 <kaqsa5579@gmail.com>
2025-03-24 09:50:03 +0500

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -80,18 +80,7 @@ class MyClass: x = 5
 p1 = MyClass() 
 print(p1.x) 
 
-'''in_it fuction and object method
-class Person: 
- def _init_ (self,name, age):
-  self.name = name 
-  self.age = age 
- def intro(self):
-    print("hello my name is"+ self.name )
-p2=Person("John", 36) 
-print(p2.name) 
-print(p2.age)
-p2.intro()'''
-
+#object method
 print("Object function")
 class Person:
   def __init__ (self, name, age): 
